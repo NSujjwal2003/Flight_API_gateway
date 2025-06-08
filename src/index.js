77b5fcd485f5822cb3a3488192354284c6867ca0
NSujjwal2003 <ujjwal.22gcebcs042@galgotiacollege.edu>
2025-06-08 17:29:58 +0530

    added reverse proxy

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const rateLimit = require('express-rate-limit');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 
@@ -13,6 +14,24 @@ const limiter = rateLimit({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(limiter);
+
+app.use('/flightsService', createProxyMiddleware({
+    target: ServerConfig.FLIGHT_SERVICE, 
+    changeOrigin: true,
+    pathRewrite: {
+        '^/flightsService': '/', // Remove the base path when forwarding to the target
+    }
+}));
+
+app.use('/BookingService', createProxyMiddleware({
+    target: ServerConfig.BOOKING_SERVICE,
+    changeOrigin: true,
+    pathRewrite: {
+        '^/BookingService': '/', // Remove the base path when forwarding to the target
+    }
+}));
+
+
 app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
